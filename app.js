e63cbd81b31c07431a551e2f92aa1ffda2828508
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-01-24 22:11:27 +0300

    ui update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const githubForm = document.getElementById("github-form");
 const nameInput = document.getElementById("githubname");
 const clearLastUsers = document.getElementById("clear-last-users");
-
 const lastUsers = document.getElementById("last-users");
+
 const github= new Github();
+const ui = new UI();
+
 
 eventListeners();
 
@@ -29,14 +31,14 @@ function getData(e){
                 console.log("User Not Found!");
             }
             else{
-                console.log(response);
+                ui.showUserInfo(response.user);
             }
         })
         // .then(response => console.log(response.user))//user information
         // .then(response => console.log(response.repo))//repo information
         .catch(error => console.log(error));
     }
-
+    ui.clearInput();//clear input
     e.preventDefault();
 }
 function clearAllSearched(){
